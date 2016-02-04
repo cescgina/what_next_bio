9f4e68c17d59cdac18fbe19091a5e6f1daeb0612
Cesc Gilabert <cescgina@gmail.com>
2016-02-04 16:48:00 +0100

    Added support for scientific american and parsing of feeds

--- downloadRSS.php
+++ downloadRSS.php
@@ -1,46 +1,97 @@
 <?php
-    $host="localhost";
+    //$host="localhost";
+    /*$host="mmb.pcb.ub.es";
     $user = "dbw09";
     $pass = "dbw2016";
     $dbname = "DBW09";
     $db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $pass);
+    */
 
     date_default_timezone_set('Europe/Madrid');
     $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));//not sure what does, without it BAD GATEWAY error
     stream_context_set_default($opts);//set requests with previous line options by default
     
-    set_time_limit(0);// not sure
+    set_time_limit(0);// removes max execution time
 
-    $urlnat = "http://feeds.nature.com/naturejobs/rss/sciencejobs";
-    $rssnat = simplexml_load_file($urlnat);
-
-    foreach($rssnat->channel->item as $itemnat) {
-            $stmtnat = $db->prepare("INSERT INTO jobs(title,link,description,date) VALUES(:title,:link,:description,:date)");
-            $stmtnat->execute(array(':title' => $itemnat->title, ':link' => $itemnat->link,':description'=> $itemnat->description, ':date'=> date("Y/m/d")));
-            //$affected_rows = $stmt->rowCount();
-    }
     $urleur= "http://ec.europa.eu/euraxess/rssFP.cfm?&idResearchField=12476878&idResPopulation=12477307";
     $streur=file_get_contents($urleur);
     $rsseur=simplexml_load_string($streur);
     foreach($rsseur->item as $itemeur) {
-            $stmteur = $db->prepare("INSERT INTO jobs(title,link,description,date) VALUES(:title,:link,:description,:date)");
-            $stmteur->execute(array(':title' => $itemeur->title, ':link' => $itemeur->link,':description'=> $itemeur->description, ':date'=> date("Y/m/d")));
+            $desca=explode("\n",$itemeur->description); 
+            $n=count($desca);
+            $pos_org=0;
+            while ($pos_org < $n){
+                if (preg_match("/Organisation/",$desca[$pos_org])){
+                    break;                    
+                }
+                $pos_org++;
+            }
+            $institution=preg_replace("/.*Organisation:\s+/","",$desca[$pos_org]);
+            while ($pos_org < $n){
+                if (preg_match("/Application/",$desca[$pos_org])){
+                    break;
+                }
+                $pos_org++;
+            }
+            $appdeadline=preg_replace("/.*Application.*:\s+/","",$desca[$pos_org]);
+           // $stmt = $db->prepare("SELECT link FROM jobs WHERE link=:link");
+            $stmt = $db->prepare("SELECT link FROM demo WHERE link=:link");
+            $stmt->execute(array(':link'=>$itemeur->link));
+            $affeccted_rows=$stmt->rowCount();
+            if ($affected_rows == 0){
+               //$stmteur = $db->prepare("INSERT INTO jobs(title,link,description,date) VALUES(:title,:link,:description,:date)");
+               $stmteur = $db->prepare("INSERT INTO demo(title,link,description,location,date) VALUES(:title,:link,:description,:location,:date)");
+            $stmteur->execute(array(':title' => $itemeur->title, ':link' => $itemeur->link,':description'=> $itemeur->description,':location'=>$institution, ':date'=> date("Y/m/d")));
+            }
+            else{
+                continue;
+            }
     }
+    $urlsci="http://jobs.sciencecareers.org/jobsrss/?Discipline=1&countrycode=US";
+    $rssci=simplexml_load_file($urlsci);
+    foreach($rssci->channel->item as $itemsci){
+            $desca=explode("\n",$itemsci->description);
+            $titles=explode(":",$itemsci->title,2);
+            $institution=$titles[0];
+            $title=$titles[1];
+            $location_pos=count($desca)-2;
+            $location=$desca[$location_pos];
+            $desc=implode("\n",array_slice($desca,0,$location_pos));
+            if ($desc[count($desc)-1] != "."){
+                $desc .= "...";
+            }
 
-    $db=null; //Close the connection to the database
-/* 
-PREPARED STATEMENTS
-$stmt = $db->prepare("INSERT INTO table(field1,field2) VALUES(:field1,:field2)");
-$stmt->execute(array(':field1' => $field1, ':field2' => $field2));
-$affected_rows = $stmt->rowCount();
-
-$stmt = $db->prepare("DELETE FROM table WHERE id=:id");
-    $stmt->bindValue(':id', $id, PDO::PARAM_STR);
-    $stmt->execute();
-    $affected_rows = $stmt->rowCount();
+            //$stmt = $db->prepare("SELECT link FROM jobs WHERE link=:link");
+            $stmt = $db->prepare("SELECT link FROM demo WHERE link=:link");
+            $stmt->execute(array(':link'=>$itemsci->link));
+            $affeccted_rows=$stmt->rowCount();
+            if ($affected_rows == 0){
+               //$stmtsci = $db->prepare("INSERT INTO jobs(title,link,description,date) VALUES(:title,:link,:description,:date)");
+               $stmtsci = $db->prepare("INSERT INTO demo(title,link,description,location,date) VALUES(:title,:link,:description,:date)");
+                $stmtsci->execute(array(':title' => $itemsci->title, ':link' => $itemsci->link,':description'=> $desc, ':location'=>$location,':date'=> date("Y/m/d")));
+            }
+            else{
+                continue;
+            }
+    }
     
-$stmt = $db->prepare("UPDATE table SET name=? WHERE id=?");
-    $stmt->execute(array($name, $id));
-    $affected_rows = $stmt->rowCount();
-*/
+    /*
+    $urlnat = "http://feeds.nature.com/naturejobs/rss/sciencejobs";
+    $rssnat = simplexml_load_file($urlnat);
+
+    foreach($rssnat->channel->item as $itemnat) {
+        
+            $stmt = $db->prepare("SELECT link FROM jobs WHERE link=:link");
+            $stmt->execute(array(':link'=>$itemnat->link));
+            $affeccted_rows=$stmt->rowCount();
+            if ($affected_rows == 0){
+                $stmtnat = $db->prepare("INSERT INTO proves(title,link,description,date) VALUES(:title,:link,:description,:date)");
+                $stmtnat->execute(array(':title' => $itemnat->title, ':link' => $itemnat->link,':description'=> $itemnat->description, ':date'=> date("Y/m/d")));
+            }
+            else {
+                echo "<h1>This record from nature already present in the db</h1>";
+            }            
+    }
+    */
+    $db=null; //Close the connection to the database
 ?>
