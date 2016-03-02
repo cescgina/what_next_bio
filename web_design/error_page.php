0507add2f99a73f5c5acb52aaaad14e083e47fee
marcemezue <marcemezue@gmail.com>
2016-03-02 14:44:55 +0100

    Supposedly corrected error page

--- web_design/error_page.php
+++ web_design/error_page.php
@@ -1,31 +1,17 @@
 <?php
 require_once 'dbconfig.php';
 include('header.php');
-
 $error = $_GET['error'];
 $linkprev = $_GET['link'];
 echo '
+	<div id="page">
 	<br><br>
 	<p>'.$error.'</p>
 	<br>
 	<a href='.$linkprev.'>Return to form</a> 
+	</div>
 	';
 
 ?>
 
-<div id="nav">
-<?php
-	echo '
-		<br>
-		<form action="login.php">
-			<input class="Button" type="submit" value="Log In"/></br>
-		</form>
-		<br>
-		<form action="register.php">
-			<input class="Button" type="submit" value="Register"/>
-		</form>
-		<br>
-	';
-?>
-</div>
 <?php include('footer.php');?>
